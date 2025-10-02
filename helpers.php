8a80a929f64c9078dfc135f62139261e60ed7521
shohan <ashiqur.shohan@gmail.com>
2025-10-03 02:56:04 +0600

    add healpers function

--- helpers.php
+++ helpers.php
@@ -7,9 +7,75 @@
  * @return string 
  */
 
-function basePath($path = '')
-{
+function basePath($path = ''){
     // __DIR__ is a magic constant
     // always gives the directory path of the file in which it is written (not the file that called it)
     return __DIR__ . '/' . $path;
+}
+
+
+/**
+ * Load View
+ * 
+ * @param string $name
+ * @return void
+ */
+
+function loadView($name) {
+    $viewPath = basePath("views/{$name}.php");
+
+    if(file_exists($viewPath)){
+        require $viewPath;
+    }
+    else{
+        echo "View {$viewPath} not found";
+    }
+}
+
+
+/**
+ * Load Partials
+ * 
+ * @param string $name
+ * @return void
+ */
+
+function loadPartials($name) {
+    $partialPath = basePath("views/partials/{$name}.php");
+
+    if(file_exists($partialPath)){
+        require $partialPath;
+    }
+    else{
+        echo "Partial {$partialPath} not found";
+    }
+}
+
+/**
+ * Inspect a Value(s)
+ * 
+ * @param mixed $value
+ * @return void
+ */
+
+function inspect($value){
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+}
+
+/**
+ * Inspect a Value(s) and Die
+ * 
+ * @param mixed $value
+ * @return void
+ */
+
+function inspectAndDie($value){
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+    
+    // immediately terminate the execution of the current script
+    die();
 }
