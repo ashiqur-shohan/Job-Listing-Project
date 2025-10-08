a9b19e2bda711ccaf2310f4319023d7c327cb93e
shohan <ashiqur.shohan@gmail.com>
2025-10-08 12:09:57 +0600

    added routes

--- public/index.php
+++ public/index.php
@@ -3,4 +3,15 @@ require '../helpers.php';
 
 // require basePath()'../views/home.php';
 
-loadView('home');
+// loadView('home');
+
+$routes = [
+    '/' => 'controllers/home.php',
+    '/listings' => 'controllers/listings/index.php',
+    '/listings/create' => 'controllers/listings/create.php',
+    '404' => 'controllers/error/404.php'
+];
+
+$uri = $_SERVER['REQUEST_URI'];
+
+inspectAndDie($uri);
