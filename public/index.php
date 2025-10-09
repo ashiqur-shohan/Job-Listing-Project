b0a7169eef6c0ac6e3b6080465555db32e9bb7ca
shohan <ashiqur.shohan@gmail.com>
2025-10-09 11:10:02 +0600

    index file refactor

--- public/index.php
+++ public/index.php
@@ -1,21 +1,9 @@
 <?php
 require '../helpers.php';
 
-// define the routes
-$routes = [
-    '/' => 'controllers/home.php',
-    '/listings' => 'controllers/listings/index.php',
-    '/listings/create' => 'controllers/listings/create.php',
-    '404' => 'controllers/error/404.php'
-];
-
 // grab the URI
 $uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
 
-// check if the route exists, else load 404
-if(array_key_exists($uri, $routes)){
-    require basePath($routes[$uri]);
-}
-else{
-    require basePath($routes['404']);
-}
+
+require basePath('router.php');
