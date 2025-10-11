1d000b1ec08b541317aef1622666cdee2202a6c7
shohan <ashiqur.shohan@gmail.com>
2025-10-11 14:46:51 +0600

    routes created by router class

--- routes.php
+++ routes.php
@@ -1,8 +1,6 @@
 <?php
-return
-[
-'/' => 'controllers/home.php',
-'/listings' => 'controllers/listings/index.php',
-'/listings/create' => 'controllers/listings/create.php',
-'404' => 'controllers/error/404.php'
-];
+
+// Create routes using the $router object
+$router->get('/', 'controllers/home.php');
+$router->get('/listings', 'controllers/listings/index.php');
+$router->get('/listings/create', 'controllers/listings/create.php');
