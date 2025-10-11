ab5eade53cf65474746b1a9e082d0c5f72baa6f8
shohan <ashiqur.shohan@gmail.com>
2025-10-11 14:42:09 +0600

    data fetch from database and pass to view

--- controllers/listings/index.php
+++ controllers/listings/index.php
@@ -1,3 +1,7 @@
 <?php
+$config = require basePath('config/db.php');
+$db = new Database($config);
 
-loadView('listings/index');
+$listings = $db->query("SELECT * FROM listings LIMIT 6")->fetchAll();
+
+loadView('listings/index', ['listings' => $listings]);
