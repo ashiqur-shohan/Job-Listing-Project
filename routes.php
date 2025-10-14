da68564c9363684eac0f44c1c5b1f0669c639d04
shohan <ashiqur.shohan@gmail.com>
2025-10-14 15:06:09 +0600

    delete listing route added

--- routes.php
+++ routes.php
@@ -6,5 +6,6 @@ $router->get('/','HomeController@index');
 $router->get('/listings','ListingController@index');
 $router->get('/listings/create','ListingController@create');
 // {id} is a route parameter
-$router->get('/listing/{id}','ListingController@show');
+$router->get('/listings/{id}','ListingController@show');
 $router->post('/listings', 'ListingController@store');
+$router->delete('/listings/{id}', 'ListingController@destroy');
