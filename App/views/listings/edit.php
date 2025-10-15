b0eb56697cc2d088268ca44bc408a4952db257ec
shohan <ashiqur.shohan@gmail.com>
2025-10-15 15:07:54 +0600

    user views created login and registration

--- App/views/listings/edit.php
+++ App/views/listings/edit.php
@@ -7,17 +7,14 @@
 <section class="flex justify-center items-center mt-20">
     <div class="bg-white p-8 rounded-lg shadow-md w-full md:w-600 mx-6">
         <h2 class="text-4xl text-center font-bold mb-4">Edit Job Listing</h2>
-        
-        <form method="POST" action="/listings/<?= $listing->id ?>" >
+
+        <form method="POST" action="/listings/<?= $listing->id ?>">
             <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">
                 Job Info
             </h2>
             <input type="hidden" name="_method" value="PUT">
-            <?php if (isset($errors)): ?>
-                <?php foreach ($errors as $error): ?>
-                    <div class="message bg-red-100 p-3 my-3"><?= $error ?></div>
-                <?php endforeach; ?>
-            <?php endif; ?>
+            <?= loadPartials('errors', ['errors' => $errors ?? []]) ?>
+
             <div class="mb-4">
                 <input type="text" name="title" placeholder="Job Title"
                     class="w-full px-4 py-2 border rounded focus:outline-none" value="<?= $listing->title ?? '' ?>" />
@@ -28,8 +25,7 @@
             </div>
             <div class="mb-4">
                 <input type="text" name="salary" placeholder="Annual Salary"
-                    class="w-full px-4 py-2 border rounded focus:outline-none"
-                    value="<?= $listing->salary ?? '' ?>" />
+                    class="w-full px-4 py-2 border rounded focus:outline-none" value="<?= $listing->salary ?? '' ?>" />
             </div>
             <div class="mb-4">
                 <input type="text" name="requirements" placeholder="Requirements"
@@ -50,13 +46,11 @@
             </h2>
             <div class="mb-4">
                 <input type="text" name="company" placeholder="Company Name"
-                    class="w-full px-4 py-2 border rounded focus:outline-none"
-                    value="<?= $listing->company ?? '' ?>" />
+                    class="w-full px-4 py-2 border rounded focus:outline-none" value="<?= $listing->company ?? '' ?>" />
             </div>
             <div class="mb-4">
                 <input type="text" name="address" placeholder="Address"
-                    class="w-full px-4 py-2 border rounded focus:outline-none"
-                    value="<?= $listing->address ?? '' ?>" />
+                    class="w-full px-4 py-2 border rounded focus:outline-none" value="<?= $listing->address ?? '' ?>" />
             </div>
             <div class="mb-4">
                 <input type="text" name="city" placeholder="City"
